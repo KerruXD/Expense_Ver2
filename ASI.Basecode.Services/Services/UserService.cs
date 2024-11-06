d0bb102be461de0de564946d271f873e00775d10
KerruXD <kerrharveycananua@gmail.com>
2024-11-06 19:22:21 +0800

    first commit

---
+++ ASI.Basecode.Services/Services/UserService.cs
@@ -0,0 +1,67 @@
+﻿using ASI.Basecode.Data;
+using ASI.Basecode.Data.Interfaces;
+using ASI.Basecode.Data.Models;
+using ASI.Basecode.Services.Interfaces;
+using ASI.Basecode.Services.Manager;
+using ASI.Basecode.Services.ServiceModels;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+using System.Linq;
+using static ASI.Basecode.Resources.Constants.Enums;
+
+namespace ASI.Basecode.Services.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly DbSet<User> _users;
+
+        public UserService(IUserRepository repository, IUnitOfWork unitOfWork, AsiBasecodeDBContext dbContext, IMapper mapper)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _users = dbContext.Users;
+        }
+
+        public LoginResult AuthenticateUser(string userEmail, string password, ref User user)
+        {
+            user = new User();
+            var passwordKey = PasswordManager.EncryptPassword(password);
+            user = _repository.GetUsers().FirstOrDefault(x => x.Email == userEmail && x.Password == passwordKey);
+
+            return user != null ? LoginResult.Success : LoginResult.Failed;
+        }
+
+        public void AddUser(UserViewModel model)
+        {
+            var user = new User();
+            if (!_repository.UserExists(model.Email))
+            {
+                _mapper.Map(model, user);
+                user.Password = PasswordManager.EncryptPassword(model.Password);
+                user.CreatedTime = DateTime.Now;
+                user.UpdatedTime = DateTime.Now;
+                user.CreatedBy = Environment.UserName;
+                user.UpdatedBy = Environment.UserName;
+
+                _repository.AddUser(user);
+                _unitOfWork.SaveChanges(); // Commit the new user to the database
+            }
+            else
+            {
+                throw new InvalidDataException(Resources.Messages.Errors.UserExists);
+            }
+        }
+
+        public void UpdateUser(User user)
+        {
+            _users.Update(user);           // Track the user entity for updates
+            _unitOfWork.SaveChanges();      // Save changes and handle concurrency
+        }
+    }
+}
